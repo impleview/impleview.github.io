0e46887dad9c8836b0d781ab1ca889b803d2c2c8
impleview <info@impleview.com>
2025-03-14 11:42:00 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@
   
     // Scroll to top button appear
     $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
+      var scrollDistance = $(this).scrollTop(); 
       if (scrollDistance > 100) {
         $('.scroll-to-top').fadeIn();
       } else {
